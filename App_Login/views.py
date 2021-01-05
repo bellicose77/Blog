adfca815ac3e6f216aeccf2b8e1896752876af87
bellicose77 <16201082@uap-bd.edu>
2021-01-05 12:15:39 +0600

    logout and password change page

--- App_Login/views.py
+++ App_Login/views.py
@@ -4,15 +4,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
+
 # Create your views here.
 from django.urls import reverse
 
+from App_Login.forms import SignUpForm, UserProfileChange
+
 
 def sign_up(request):
-    form = UserCreationForm()
+    form = SignUpForm()
     registered = False
     if request.method == 'POST':
-        form =UserCreationForm(data=request.POST)
+        form =SignUpForm(data=request.POST)
         if form.is_valid():
             form.save()
             registered = True
@@ -37,3 +40,20 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+@login_required
+def profile(request):
+    return render(request, 'App_Login/profile.html', context={})
+
+
+
+@login_required
+def user_change(request):
+    current_user = request.user
+    form = UserProfileChange(instance=current_user)
+    if request.method == 'POST':
+        form = UserProfileChange(request.POST, instance=current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance=current_user)
+    return render(request, 'App_Login/change_profile.html', context={'form':form})
